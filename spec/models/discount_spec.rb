f3a0f601f05fdf58de7fc884d9b240aebc7eac30
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-09-19 18:30:32 -0600

    created validations and used shoulda matchers

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Discount do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
+  describe 'Validations' do
+    it do
+      should validate_numericality_of(:percent_off).
+      is_less_than_or_equal_to(100)
+    end
+    it do
+      should validate_numericality_of(:min_quantity).
+      is_greater_than(0)
+    end
+    it {should validate_presence_of :name}
+  end
 end
